13e7e8db3e8d315b8eb14b61e3f1de67f0c35ee4
Jake Weinstein <xboxlover360@gmail.com>
2016-08-01 13:57:12 -0400

    libm: add optimized expf/logf/powf from ARM
    * Taken from here: https://github.com/arm-software/optimized-routines
    
    Change-Id: I18c22f1a3a2c02180eee818f2f6cb8777f4bcc69
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libm/Android.mk
+++ libm/Android.mk
@@ -22,6 +22,7 @@ LOCAL_SRC_FILES := \
     upstream-freebsd/lib/msun/bsdsrc/b_tgamma.c \
     upstream-freebsd/lib/msun/src/catrig.c \
     upstream-freebsd/lib/msun/src/catrigf.c \
+    upstream-freebsd/lib/msun/src/dunder.c \
     upstream-freebsd/lib/msun/src/e_acos.c \
     upstream-freebsd/lib/msun/src/e_acosf.c \
     upstream-freebsd/lib/msun/src/e_acosh.c \
@@ -72,6 +73,7 @@ LOCAL_SRC_FILES := \
     upstream-freebsd/lib/msun/src/e_sinhf.c \
     upstream-freebsd/lib/msun/src/e_sqrt.c \
     upstream-freebsd/lib/msun/src/e_sqrtf.c \
+    upstream-freebsd/lib/msun/src/funder.c \
     upstream-freebsd/lib/msun/src/imprecise.c \
     upstream-freebsd/lib/msun/src/k_cos.c \
     upstream-freebsd/lib/msun/src/k_cosf.c \
