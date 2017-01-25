4648337169a0361bf14be796b66c7d02c3ccfdf9
Dimitry Ivanov <dimitry@google.com>
2017-01-25 14:07:51 -0800

    Increase map size for block allocator to 5 pages
    This reduces number of mprotect calls for large executables
    like app_process. The downside is that it increases address
    space consumption for small executables like cat.
    
    Bug: http://b/34404021
    Test: boot a device, run linker-unit-tests
    Change-Id: I6391f857af2fd97389a2b56495c7410a57acbf6c
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- linker/linker_block_allocator.cpp
+++ linker/linker_block_allocator.cpp
@@ -34,6 +34,8 @@
 
 #include "private/bionic_prctl.h"
 
+static const constexpr size_t kBlockAllocatorMapSize = 5 * PAGE_SIZE;
+
 // the multiplier should be power of 2
 static constexpr size_t round_up(size_t size, size_t multiplier) {
   return (size + (multiplier - 1)) & ~(multiplier-1);
@@ -41,7 +43,7 @@ static constexpr size_t round_up(size_t size, size_t multiplier) {
 
 struct LinkerBlockAllocatorPage {
   LinkerBlockAllocatorPage* next;
-  uint8_t bytes[PAGE_SIZE - 16] __attribute__((aligned(16)));
+  uint8_t bytes[kBlockAllocatorMapSize - 16] __attribute__((aligned(16)));
 };
 
 struct FreeBlockInfo {
@@ -106,28 +108,34 @@ void LinkerBlockAllocator::free(void* block) {
 
 void LinkerBlockAllocator::protect_all(int prot) {
   for (LinkerBlockAllocatorPage* page = page_list_; page != nullptr; page = page->next) {
-    if (mprotect(page, PAGE_SIZE, prot) == -1) {
+    if (mprotect(page, kBlockAllocatorMapSize, prot) == -1) {
       abort();
     }
   }
 }
 
 void LinkerBlockAllocator::create_new_page() {
-  static_assert(sizeof(LinkerBlockAllocatorPage) == PAGE_SIZE,
+  static_assert(sizeof(LinkerBlockAllocatorPage) == kBlockAllocatorMapSize,
                 "Invalid sizeof(LinkerBlockAllocatorPage)");
 
   LinkerBlockAllocatorPage* page = reinterpret_cast<LinkerBlockAllocatorPage*>(
-      mmap(nullptr, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0));
+      mmap(nullptr,
+           kBlockAllocatorMapSize,
+           PROT_READ|PROT_WRITE,
+           MAP_PRIVATE|MAP_ANONYMOUS|MAP_NORESERVE,
+           0,
+           0));
 
   if (page == MAP_FAILED) {
     abort(); // oom
   }
 
-  prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, page, PAGE_SIZE, "linker_alloc");
+  prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, page, kBlockAllocatorMapSize, "linker_alloc");
 
   FreeBlockInfo* first_block = reinterpret_cast<FreeBlockInfo*>(page->bytes);
   first_block->next_block = free_block_list_;
-  first_block->num_free_blocks = (PAGE_SIZE - sizeof(LinkerBlockAllocatorPage*))/block_size_;
+  first_block->num_free_blocks =
+      (kBlockAllocatorMapSize - sizeof(LinkerBlockAllocatorPage*))/block_size_;
 
   free_block_list_ = first_block;
 
@@ -143,7 +151,7 @@ LinkerBlockAllocatorPage* LinkerBlockAllocator::find_page(void* block) {
   LinkerBlockAllocatorPage* page = page_list_;
   while (page != nullptr) {
     const uint8_t* page_ptr = reinterpret_cast<const uint8_t*>(page);
-    if (block >= (page_ptr + sizeof(page->next)) && block < (page_ptr + PAGE_SIZE)) {
+    if (block >= (page_ptr + sizeof(page->next)) && block < (page_ptr + kBlockAllocatorMapSize)) {
       return page;
     }
 
