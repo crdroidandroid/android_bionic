99babf245d461d94f3167aab4b94247a86f072cf
Jake Weinstein <xboxlover360@gmail.com>
2017-05-06 23:22:58 -0400

    libc: import ARM strcmp from newlib
    * Current version is also based on newlib, but an older revision
    
    * The Krait and A9 specific changes no longer seem relevant, so
      let's use A15 directly.
    
    * Add strcmp benchmark to bionic benchmarks
    
    Tested on OnePlus 3 (MSM8996):
    
    Before (Krait strcmp):
                                           iterations      ns/op
    BM_string_strcmp/8                          1000k         24    0.322 GiB/s
    BM_string_strcmp/64                           20M        123    0.519 GiB/s
    BM_string_strcmp/512                           2M        920    0.556 GiB/s
    BM_string_strcmp/1024                       1000k       1818    0.563 GiB/s
    BM_string_strcmp/8Ki                         200k      14405    0.569 GiB/s
    BM_string_strcmp/16Ki                        100k      28762    0.570 GiB/s
    BM_string_strcmp/32Ki                         50k      57526    0.570 GiB/s
    BM_string_strcmp/64Ki                         10k     114959    0.570 GiB/s
    
    After:
                                           iterations      ns/op
    BM_string_strcmp/8                          1000k         28    0.284 GiB/s
    BM_string_strcmp/64                           20M        107    0.596 GiB/s
    BM_string_strcmp/512                           2M        800    0.639 GiB/s
    BM_string_strcmp/1024                       1000k       1579    0.649 GiB/s
    BM_string_strcmp/8Ki                         200k      12469    0.657 GiB/s
    BM_string_strcmp/16Ki                        100k      24931    0.657 GiB/s
    BM_string_strcmp/32Ki                         50k      49843    0.657 GiB/s
    BM_string_strcmp/64Ki                         20k      99635    0.658 GiB/s
    
    Test: bionic-benchmarks BM_string_memcmp
    Change-Id: Icb3bfb0a381bcc1e10885ca5e9547842c3f620d7
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -19,7 +19,7 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a9/bionic/stpcpy.S \
     arch-arm/cortex-a9/bionic/strcat.S \
     arch-arm/cortex-a9/bionic/__strcat_chk.S \
-    arch-arm/cortex-a9/bionic/strcmp.S \
+    arch-arm/cortex-a15/bionic/strcmp.S \
     arch-arm/cortex-a9/bionic/strcpy.S \
     arch-arm/cortex-a9/bionic/__strcpy_chk.S \
     arch-arm/cortex-a9/bionic/strlen.S \
