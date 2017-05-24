e3f34b95eef085d691b336515fc8b66a082ec90f
Pranav Vashi <neobuddy89@gmail.com>
2017-05-24 22:39:33 +0530

    libc: Use cortex-a15 strcmp for kryo and scorpion
    Change-Id: I4b99b2353baa8b73833c3b4947a1d802bc74cb96
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libc/arch-arm/kryo/kryo.mk
+++ libc/arch-arm/kryo/kryo.mk
@@ -20,12 +20,12 @@ libc_bionic_src_files_arm += \
 libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/cortex-a15/bionic/strcat.S \
+    arch-arm/cortex-a15/bionic/strcmp.S \
     arch-arm/cortex-a15/bionic/strcpy.S \
     arch-arm/cortex-a15/bionic/strlen.S \
 
 # Use krait versions of strcmp
 libc_bionic_src_files_arm += \
-    arch-arm/krait/bionic/strcmp.S \
     arch-arm/krait/bionic/__strcat_chk.S \
     arch-arm/krait/bionic/__strcpy_chk.S \
 
