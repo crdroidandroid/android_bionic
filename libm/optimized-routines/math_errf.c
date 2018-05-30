cbf96db33add262b69b6c78cce409a6d4893fddb
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2018-05-30 13:48:17 -0300

    libm: Add optimized expf
    This implementation is based on code from ARM optimized routines [1].
    The error for expf, for nearest rouding mode, is estimated on 0.502 ulp
    with worst case in non-nearest up to 1 ulp.
    
    Double precision arithmetics is used with internal calculation and it
    is expected to be faster and easier to get better precision on most
    targets.  The const data is kept in a separate translation unit that
    allows fast code for literal loads on most targets and also allows
    sharing data across expf, exp2f and powf.
    
    The performance shows about 2x better throughput and 1.8x better
    latency for aarch64 (A72) using expf benchmark.
    
    Some details may need target specific tweaks:
    
     * Best convert and round to int operation in the arg reduction may be
       different across targets.  This is defined by two preprocessors:
    
       - HAVE_FAST_ROUND: which indicates the target have hardware
         instruction for the round operation.
    
       - HAVE_FAST_LROUND: as before, but for lround operation.
    
       Currently aarch64 is defined to have support for both.
    
     * An extra configuration file is added for bionic (bionic.h) to correct
       set the library expectations regarding rounding and errno setting.
    
     * Code was optimized for targets with FMA instruction (optimal
       polynomial eval may be different without fma).
    
     * Compiler might not always generate good code for floating bit
       representation access via unions or it may be inherently slow on some
       targets.
    
    [1] https://github.com/ARM-software/optimized-routines
    
    Test: ran 32-bit and 64-bit x86 tests on host
    Change-Id: If67293b450e32666c4d40482ea0380f661ad5581
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ libm/optimized-routines/math_errf.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2017, Arm Limited.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "math_config.h"
+
+#if WANT_ERRNO
+#include <errno.h>
+/* NOINLINE reduces code size and avoids making math functions non-leaf
+   when the error handling is inlined.  */
+NOINLINE static float
+with_errnof (float y, int e)
+{
+  errno = e;
+  return y;
+}
+#else
+#define with_errnof(x, e) (x)
+#endif
+
+/* NOINLINE reduces code size.  */
+NOINLINE static float
+xflowf (uint32_t sign, float y)
+{
+  y = opt_barrier_float (sign ? -y : y) * y;
+  return with_errnof (y, ERANGE);
+}
+
+HIDDEN float
+__math_uflowf (uint32_t sign)
+{
+  return xflowf (sign, 0x1p-95f);
+}
+
+#if WANT_ERRNO_UFLOW
+/* Underflows to zero in some non-nearest rounding mode, setting errno
+   is valid even if the result is non-zero, but in the subnormal range.  */
+HIDDEN float
+__math_may_uflowf (uint32_t sign)
+{
+  return xflowf (sign, 0x1.4p-75f);
+}
+#endif
+
+HIDDEN float
+__math_oflowf (uint32_t sign)
+{
+  return xflowf (sign, 0x1p97f);
+}
+
+HIDDEN float
+__math_divzerof (uint32_t sign)
+{
+  float y = opt_barrier_float (sign ? -1.0f : 1.0f) / 0.0f;
+  return with_errnof (y, ERANGE);
+}
+
+HIDDEN float
+__math_invalidf (float x)
+{
+  float y = (x - x) / (x - x);
+  return isnan (x) ? y : with_errnof (y, EDOM);
+}
