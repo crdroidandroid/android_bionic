a956fe68ebf3fd86bffe6936924a475a96951583
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2018-06-08 07:38:43 -0300

    Add Optimized sinf/cosf/sincosf
    This implementation is based on code from ARM optimized routines [1].
    The worst-case ULP is 0.56072, maximum relative error is 0.5303p-23
    over all 4 billion inputs.  In non-nearest rounding modes the error
    is 1ULP.
    
    The algorithm uses 3 main cases: small inputs which don't need argument
    reduction, small inputs which need a simple range reduction and large
    inputs requiring complex range reduction.  The code uses approximate
    integer comparisons to quickly decide between these cases - on some
    targets this may be slow, so this can be configured to use floating
    point comparisons.
    
    The small range reducer uses a single reduction step to handle values
    up to 120.0.  It is fastest on targets which support inlined round
    instructions (such as aarch64).
    
    The large range reducer uses integer arithmetic for simplicity.  It does
    a 32x96 bit multiply to compute a 64-bit modulo result.  This is more
    than accurate enough to handle the worst-case cancellation for values
    close to an integer multiple of PI/4.  It could be further optimized,
    however it is already much faster than necessary.
    
    The performance improvements are for the ranges:
    
    * sinf:
    
    reciprocal-throughput    | speedup
                             |
    workload-0.0<=x<0.1      |  1.3912
    workload-0.1<=x<0.7      |  1.3758
    workload-0.7<=x<3.1      |  0.8562
    workload--3.1<=x<3.1     |  0.9086
    workload-3.3<=x<33.3     |  1.5010
    workload-100.0<=x<1000.0 |  1.3633
    workload-1e6<=x<1e32     |  7.5280
    workload-1e32<=x<FLT_MAX |  7.6309
    
    latency                  | speedup
                             |
    workload-0.0<=x<0.1      |  1.1520
    workload-0.1<=x<0.7      |  1.1551
    workload-0.7<=x<3.1      |  0.8741
    workload--3.1<=x<3.1     |  0.8901
    workload-3.3<=x<33.3     |  1.2088
    workload-100.0<=x<1000.0 |  0.9458
    workload-1e6<=x<1e32     |  4.4987
    workload-1e32<=x<FLT_MAX |  4.5011
    
    * cosf:
    
    reciprocal-throughput    | speedup
                             |
    workload-0.0<=x<0.1      |  1.3905
    workload-0.1<=x<0.7      |  1.3914
    workload-0.7<=x<3.1      |  0.8934
    workload--3.1<=x<3.1     |  1.0384
    workload-3.3<=x<33.3     |  1.5087
    workload-100.0<=x<1000.0 |  1.4406
    workload-1e6<=x<1e32     |  7.5917
    workload-1e32<=x<FLT_MAX |  7.7345
    
    latency                  | speedup
                             |
    workload-0.0<=x<0.1      |  1.0307
    workload-0.1<=x<0.7      |  1.0403
    workload-0.7<=x<3.1      |  0.9073
    workload--3.1<=x<3.1     |  0.9587
    workload-3.3<=x<33.3     |  1.1769
    workload-100.0<=x<1000.0 |  0.9564
    workload-1e6<=x<1e32     |  4.5323
    workload-1e32<=x<FLT_MAX |  4.5767
    
    *sincosf
    
    reciprocal-throughput    | speedup
                             |
    workload-0.0<=x<0.1      |  1.3115
    workload-0.1<=x<0.7      |  1.3050
    workload-0.7<=x<3.1      |  1.1567
    workload-3.3<=x<33.3     |  1.6762
    workload--3.1<=x<3.1     |  1.0834
    workload-100.0<=x<1000.0 |  1.2126
    workload-1e6<=x<1e32     |  5.8841
    workload-1e32<=x<FLT_MAX |  5.9366
    
    latency                  | speedup
                             |
    workload-0.0<=x<0.1      |  1.1481
    workload-0.1<=x<0.7      |  1.1461
    workload-0.7<=x<3.1      |  1.0668
    workload-3.3<=x<33.3     |  1.5334
    workload--3.1<=x<3.1     |  0.9935
    workload-100.0<=x<1000.0 |  1.0608
    workload-1e6<=x<1e32     |  4.9349
    workload-1e32<=x<FLT_MAX |  4.9754
    
    Some ranges show regressios due missing optimization opportunities
    from not using fused multiply-add operations.  It is because LLVM
    still does not use -ffp-contract=fast as default and this is addressed
    in following patch.
    
    [1] https://github.com/ARM-software/optimized-routines
    
    Test: ran 32-bit and 64-bit x86 tests on host
    Change-Id: I742b598f7b96f9fe671be29f263122144692d87f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ libm/optimized-routines/sinf.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2018, Arm Limited.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <math.h>
+#include "math_config.h"
+#include "sincosf.h"
+
+/* Fast sinf implementation.  Worst-case ULP is 0.56072, maximum relative
+   error is 0.5303p-23.  A single-step signed range reduction is used for
+   small values.  Large inputs have their range reduced using fast integer
+   arithmetic.
+*/
+float
+sinf (float y)
+{
+  double x = y;
+  double s;
+  int n;
+  sincos_t *p = &sincosf_table[0];
+
+  if (abstop12 (y) < abstop12 (pio4))
+    {
+      s = x * x;
+
+      if (unlikely (abstop12 (y) < abstop12 (0x1p-12f)))
+      {
+	if (unlikely (abstop12 (y) < abstop12 (0x1p-126f)))
+	  /* Force underflow for tiny y.  */
+	  force_eval_float (s);
+	return y;
+      }
+
+      return sinf_poly (x, s, p, 0);
+    }
+  else if (likely (abstop12 (y) < abstop12 (120.0f)))
+    {
+      x = reduce_fast (x, p, &n);
+
+      /* Setup the signs for sin and cos.  */
+      s = p->sign[n & 3];
+
+      if (n & 2)
+	p = &sincosf_table[1];
+
+      return sinf_poly (x * s, x * x, p, n);
+    }
+  else if (abstop12 (y) < abstop12 (INFINITY))
+    {
+      uint32_t xi = asuint (y);
+      int sign = xi >> 31;
+
+      x = reduce_large (xi, &n);
+
+      /* Setup signs for sin and cos - include original sign.  */
+      s = p->sign[(n + sign) & 3];
+
+      if ((n + sign) & 2)
+	p = &sincosf_table[1];
+
+      return sinf_poly (x * s, x * x, p, n);
+    }
+  else
+    return __math_invalidf (y);
+}
