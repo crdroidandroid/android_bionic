0b3ed727259d8253b40b95fca78aed606989813f
Shibin George <shibing@codeaurora.org>
2019-05-21 12:50:10 +0530

    Avoid recording backtrace if alloc-size is outside range of interest
    Steps:
    1) setprop libc.debug.malloc.program <program name>
    2) setprop libc.debug.malloc.options "backtrace leak_track"
    3) setprop libc.debug.malloc.minalloctorecord <int> # defaults to 0
    4) setprop libc.debug.malloc.maxalloctorecord <int> # defaults to SIZE_MAX
    
    With this, backtrace recording of only those allocations which fall within
    the desired range will be done.
    
    CRs-Fixed: 2471840
    
    Change-Id: I586dfd590e429b53e7b1b237294f0298ff7b4017
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libc/malloc_debug/malloc_debug.cpp
+++ libc/malloc_debug/malloc_debug.cpp
@@ -39,6 +39,8 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
+#include <sys/system_properties.h>
+
 #include <mutex>
 #include <vector>
 
@@ -67,6 +69,10 @@ DebugData* g_debug;
 bool* g_zygote_child;
 
 const MallocDispatch* g_dispatch;
+
+size_t g_min_alloc_to_record = 0;
+size_t g_max_alloc_to_record = SIZE_MAX;
+
 // ------------------------------------------------------------------------
 
 // ------------------------------------------------------------------------
@@ -325,6 +331,23 @@ bool debug_initialize(const MallocDispatch* malloc_dispatch, bool* zygote_child,
   // Always enable the backtrace code since we will use it in a number
   // of different error cases.
   backtrace_startup();
+
+  char min_alloc_to_record[10];
+  if (__system_property_get("libc.debug.malloc.minalloctorecord", min_alloc_to_record)) {
+    g_min_alloc_to_record = atoi(min_alloc_to_record);
+  }
+
+  char max_alloc_to_record[10];
+  if (__system_property_get("libc.debug.malloc.maxalloctorecord", max_alloc_to_record)) {
+    g_max_alloc_to_record = atoi(max_alloc_to_record);
+  }
+
+  if (g_min_alloc_to_record > g_max_alloc_to_record) {
+    error_log("%s: min_alloc_to_record > max_alloc_to_record!,"
+        "reverting back to default limits", getprogname());
+    g_min_alloc_to_record = 0;
+    g_max_alloc_to_record = SIZE_MAX;
+  }
 
   if (g_debug->config().options() & VERBOSE) {
     info_log("%s: malloc debug enabled", getprogname());
