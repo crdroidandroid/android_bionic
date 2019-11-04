21d86ce29287290905387a3a3f2651dfe6356fec
Jake Weinstein <jake@aospa.co>
2019-11-04 06:47:04 +0100

    libm: add l(l)rint/l(l)round routines from musl
    Change-Id: Iea9e8093c49fb3c446f8bc458b265a4e4b889837

--- libm/upstream-freebsd/lib/msun/src/s_lroundf.c
+++ libm/upstream-freebsd/lib/msun/src/s_lroundf.c
@@ -1,11 +1,6 @@
-#include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
+#include <math.h>
 
-#define type		float
-#define	roundit		roundf
-#define dtype		long
-#define	DTYPE_MIN	LONG_MIN
-#define	DTYPE_MAX	LONG_MAX
-#define	fn		lroundf
-
-#include "s_lround.c"
+long lroundf(float x)
+{
+	return roundf(x);
+}
