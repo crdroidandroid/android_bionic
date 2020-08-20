c1d6a0eeaebdfb2dbd02f66a8d015a0756d98352
Rishiraj Manwatkar <rmanwatk@codeaurora.org>
2020-08-20 11:29:16 -0700

    Gwp-asan: Return false in ShouldGwpAsanSampleProcess always
    With this change Gwp-asan will skip sampling the process
    
    CRs-Fixed: 2768682
    Change-Id: I04946c76263799be1ad67467d708cd4e9731db5a

--- libc/bionic/gwp_asan_wrappers.cpp
+++ libc/bionic/gwp_asan_wrappers.cpp
@@ -202,12 +202,15 @@ static const MallocDispatch gwp_asan_dispatch __attribute__((unused)) = {
 // The probability (1 / kProcessSampleRate) that a process will be ranodmly
 // selected for sampling. kProcessSampleRate should always be a power of two to
 // avoid modulo bias.
-static constexpr uint8_t kProcessSampleRate = 128;
+//static constexpr uint8_t kProcessSampleRate = 128;
 
 bool ShouldGwpAsanSampleProcess() {
-  uint8_t random_number;
-  __libc_safe_arc4random_buf(&random_number, sizeof(random_number));
-  return random_number % kProcessSampleRate == 0;
+    //Returning false from this function will skip the sampling process, thus
+    //effectively disable gwp-asan.
+    /*uint8_t random_number;
+    __libc_safe_arc4random_buf(&random_number, sizeof(random_number));
+    return random_number % kProcessSampleRate == 0;*/
+    return false;
 }
 
 bool MaybeInitGwpAsanFromLibc(libc_globals* globals) {
