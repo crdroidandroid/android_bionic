a8ab5090e7612d5080811e8ec899c3c5beb5432c
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-04-06 07:23:56 +0800

    fixing adb service spoofing
    root detectors checks if the adb service props were empty or uninitialized instead of actual status
    
    Change-Id: I7757308fa2d9b52eb02f3728a3326d332511e3af
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libc/bionic/custom_rom_hide.cpp
+++ libc/bionic/custom_rom_hide.cpp
@@ -427,6 +427,8 @@ static const char* const kSpoofedEmptyProps[] = {
     "ro.modversion",
     "init.svc_debug_pid.adb_root",
     "init.svc_debug_pid.adbd",
+    "init.svc.adb_root",
+    "init.svc.adbd",
     nullptr
 };
 
@@ -442,8 +444,6 @@ static const PropOverride kSpoofedValueProps[] = {
     {"ro.adb.secure", "1"},
     {"persist.sys.usb.config", "mtp"},
     {"service.adb.root", "0"},
-    {"init.svc.adb_root", "stopped"},
-    {"init.svc.adbd", "stopped"},
     {"sys.usb.config", "mtp"},
     {nullptr, nullptr}
 };
